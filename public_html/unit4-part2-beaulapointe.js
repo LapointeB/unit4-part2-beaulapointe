6d9b5eb70d8f29905572691fe0e28776affcccba
Owner <Owner@192.168.10.107>
2020-10-26 11:44:31 -0500

    Finished the program

--- public_html/unit4-part2-beaulapointe.js
+++ public_html/unit4-part2-beaulapointe.js
@@ -16,4 +16,24 @@ for (i = 1; i < numberList.length; i++) {
     }
 }
 
+let largest = Number(numberList[0]);
+
+for (i = 1; i < numberList.length; i++) {
+    if (Number(numberList[i]) > largest) {
+        largest = Number(numberList[i]);
+    }
+}
+
+let sum = 0;
+
+for (i = 0; i < numberList.length; i++) {
+    sum = sum + Number(numberList[i]);
+}
+
+let average = sum / numberList.length;
+
+document.write("The lowest number is " + smallest + "<br>");
+document.write("The highest number is " + largest + "<br>");
+document.write("The total is " + sum + "<br>");
+document.write("The mean is " + average);
 
