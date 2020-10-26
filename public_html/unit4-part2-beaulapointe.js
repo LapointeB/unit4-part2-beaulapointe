9f854b02ea510a156e21ebf13bd8252d2ec543ba
Owner <Owner@192.168.10.107>
2020-10-26 11:37:26 -0500

    Added smallest and input

--- public_html/unit4-part2-beaulapointe.js
+++ public_html/unit4-part2-beaulapointe.js
@@ -2,5 +2,18 @@
  * ask the user to enter 20 numbers
  * find the lowest, the highest, the total, and the average of the numbers
  */
+let numberList = [];
+
+for (i = 1; i <= 20; i++) {
+    numberList.push(prompt("Enter number " + i + ": "));
+}
+
+let smallest = Number(numberList[0]);
+
+for (i = 1; i < numberList.length; i++) {
+    if (Number(numberList[i]) < smallest) {
+        smallest = Number(numberList[i]);
+    }
+}
 
 
